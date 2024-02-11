69290dd376fe809845ae8447bf4da0aae7211646
Matt <matt@threlfall.dev>
2024-02-11 21:27:34 +0000

    feat: Spell check me

--- chadrc.lua
+++ chadrc.lua
@@ -3,5 +3,7 @@ local M = {}
 M.ui = { theme = 'catppuccin', tabufline= {enabled= false} }
 M.plugins = "custom.plugins"
 M.mappings = require "custom.configs.mappings"
+vim.opt.spelllang = 'en_gb'
+vim.opt.spell = true
 
 return M
